3ffd05fba10b697375b3fd9536564d51751b1c8c
Justin Tirrell <jjttjj@gmail.com>
2014-11-24 12:24:17 -0500

    fixed error with ess-setup during init

--- setup-ess.el
+++ setup-ess.el
@@ -1,4 +1,8 @@
+(require 'ess-site)
+
 (setq ess-S-assign-key (kbd "C-="))
 (ess-toggle-S-assign-key t) ; enable above key definition
 ;; leave my underscore key alone!
 (ess-toggle-underscore nil)
+
+(provide 'setup-ess)
