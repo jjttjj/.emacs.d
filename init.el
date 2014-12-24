b525e599db2d4e20448c501838fa16350d9319f7
Justin Tirrell <jjttjj@gmail.com>
2014-12-24 13:51:50 -0500

    add erc filter commands

--- init.el
+++ init.el
@@ -103,3 +103,6 @@
 ;; Conclude init by setting up specifics for the current user
 (when (file-exists-p user-settings-dir)
   (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+
+
+(setq erc-hide-list '("JOIN" "PART" "QUIT"))
--- setup-clojure-mode.el
+++ setup-clojure-mode.el
@@ -2,6 +2,11 @@
 
 (require 'cider)
 
+;;ripped off from:
+
+
+
+;;;;
 
 ;; Indent and highlight more commands
 (put-clojure-indent 'match 'defun)
@@ -11,7 +16,9 @@
 
 
 ;; Enable error buffer popping also in the REPL:
-(setq cider-repl-popup-stacktraces t)
+(setq cider-repl-popup-stacktraces nil)
+(setq cider-show-error-buffer 'only-in-repl)
+(setq cider-stacktrace-fill-column 80)
 
 
 ;; auto-select the error buffer when it's displayed
@@ -20,5 +27,20 @@
 (setq cider-repl-pop-to-buffer-on-connect nil)
 ;; Enable eldoc in Clojure buffers
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+
+;;these maybe dont belong here
+(add-hook 'clojure-mode-hook 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'highlight-parentheses)
+(add-hook 'clojure-mode-hook 'show-paren-mode)
+
+(autoload 'clojure-mode "clojure-mode")
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljx$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.hl\\'" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
+
+
 
 (provide 'setup-clojure-mode)
